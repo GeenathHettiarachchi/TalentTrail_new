f5514f2edc81e308e448de4e0d4da85e41ff82b2
banuramacbook <banurayomaldesilva@gmail.com>
2025-09-30 10:00:18 +0530

    second commit

--- frontend/src/components/QAForm/QAForm.jsx
+++ frontend/src/components/QAForm/QAForm.jsx
@@ -12,3 +12,22 @@ import {
   FiTrash2,
 } from 'react-icons/fi';
 import styles from './QAForm.module.css';
+
+const QAForm = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  intern,               // from QA.jsx
+  editingIntern = null, // backward-compat
+  isLoading = false,
+}) => {
+  const current = useMemo(() => editingIntern ?? intern ?? null, [editingIntern, intern]);
+
+  const [formData, setFormData] = useState({
+    internCode: '',
+    name: '',
+    email: '',
+    mobileNumber: '',
+    trainingEndDate: '',
+  });
+}
